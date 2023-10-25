89ba12902c6dd659d5a0dde019f0893c3bce4cff
starfall-yuna <lyn99730@naver.com>
2023-10-25 10:51:17 +0900

    csharp

---
+++ CSharp/HelloCSharp01/HelloCSharp01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ctrl + F5 = 실행
+            Console.WriteLine("Hello World");
+            //cw 적고 tab tab
+            Console.WriteLine("안녕");
+            Console.WriteLine("방가 방가");
+            //cw : Console.WriteLine 코드의 코드 조각
+            //visual studio에서 C#을 하면 자동 완성이 잘 되어 있음
+            Console.WriteLine("이동준");
+        }
+    }
+}
---
+++ CSharp/HelloCSharp01/HelloCSharp01_02_WinForm/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharp01_02_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //mbox 적고 tab tab
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox2.Text);
+            //ctrl + d 누르면 줄 복사 됨
+            int num2 = int.Parse(textBox3.Text);
+            MessageBox.Show("두 값의 합 : " + (num1+num2));
+        }
+    }
+}
